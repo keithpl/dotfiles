e37fe6049391d248e117db6b4b0ff68bc54cd9f4
Keith Plant <kplantjr@gmail.com>
2022-11-20 10:43:34 -0500

    simplifiy zinit installation script

--- dot_config/zsh/zinit-install.zsh
+++ dot_config/zsh/zinit-install.zsh
@@ -1,18 +1,13 @@
 #!/usr/bin/env zsh
 
-if [[ ! -f "$HOME/.local/share/zinit/zinit.git/zinit.zsh" ]]; then
-	mkdir -p "$HOME/.local/share/zinit" && \
-		chmod g-rwX "$HOME/.local/share/zinit"
+ZINIT_HOME="$XDG_DATA_HOME/zinit"
+
+if [[ ! -f "$ZINIT_HOME/zinit.git/zinit.zsh" ]]; then
+	mkdir -p "$ZINIT_HOME" && chmod g-rwX "$ZINIT_HOME"
 	git clone https://github.com/zdharma-continuum/zinit \
-		"$HOME/.local/share/zinit/zinit.git"
+		"$ZINIT_HOME/zinit.git"
 fi
 
-source "$HOME/.local/share/zinit/zinit.git/zinit.zsh"
+source "$ZINIT_HOME/zinit.git/zinit.zsh"
 autoload -Uz _zinit
 (( ${+_comps} )) && _comps[zinit]=_zinit
-
-zinit light-mode for \
-	zdharma-continuum/zinit-annex-as-monitor \
-	zdharma-continuum/zinit-annex-bin-gem-node \
-	zdharma-continuum/zinit-annex-patch-dl \
-	zdharma-continuum/zinit-annex-rust
