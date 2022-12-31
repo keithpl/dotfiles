819dfd61e9485e3c9e83c22ad7f93c35de096cf4
Keith Plant <kplantjr@gmail.com>
2022-12-31 10:33:44 -0500

    nvim: added cmdheight=0 workarounds until it's fixed upstream

---
+++ dot_config/nvim/lua/config/cmdheight.lua
@@ -0,0 +1,24 @@
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+local cmdheight_fixes = augroup("cmdheight-fixes", { clear = true })
+
+autocmd({ "RecordingEnter" }, {
+    group = cmdheight_fixes,
+    pattern = "*",
+    callback = function()
+        vim.opt_local.cmdheight = 1
+    end
+})
+
+autocmd({ "RecordingLeave" }, {
+    group = cmdheight_fixes,
+    pattern = "*",
+    callback = function()
+        local timer = vim.loop.new_timer()
+
+        timer:start(10, 0, vim.schedule_wrap(function()
+            vim.opt_local.cmdheight = 0
+        end))
+    end
+})
