8b1aded01212902a2530f84b0c83b4b0dd69b11d
Keith Plant <kplantjr@gmail.com>
2023-02-18 17:24:35 -0500

    nvim: change the style of the cmp window

--- dot_config/nvim/after/plugin/completion.lua
+++ dot_config/nvim/after/plugin/completion.lua
@@ -1,5 +1,6 @@
 local res
 local cmp
+local cmp_window
 local luasnip
 local autopairs
 
@@ -14,13 +15,49 @@ if not res then
     return
 end
 
+res, cmp_window = pcall(require, "cmp.utils.window")
+if not res then
+    vim.notify("Failed to load nvim-cmp.utils.window lua module")
+    return
+end
+
 res, luasnip = pcall(require, "luasnip")
 if not res then
     vim.notify("Failed to load luasnip lua module")
     return
 end
 
+local function cmp_border(hl_name)
+    return {
+        { "╭", hl_name },
+        { "─", hl_name },
+        { "╮", hl_name },
+        { "│", hl_name },
+        { "╯", hl_name },
+        { "─", hl_name },
+        { "╰", hl_name },
+        { "│", hl_name }
+    }
+end
+
+cmp_window.info_ = cmp_window.info
+cmp_window.info = function(self)
+    local info = self:info_()
+
+    info.scrollable = false
+    return info
+end
+
 cmp.setup({
+    window = {
+        completion = {
+            border = cmp_border("CmpBorder"),
+            winhighlight = "Normal:CmpPmenu,CursorLine:PmenuSel,Search:None"
+        },
+        documentation = {
+            border = cmp_border("CmpBorder")
+        }
+    },
     snippet = {
         expand = function(args)
             luasnip.lsp_expand(args.body)
--- dot_config/nvim/after/plugin/treeshitter.lua
+++ dot_config/nvim/after/plugin/treeshitter.lua
@@ -15,6 +15,7 @@ ts_configs.setup({
     },
     highlight = {
         enable = true,
+        use_languagetree = true,
         additional_vim_regex_highlighting = false
     }
 })
